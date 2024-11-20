f3edb387af46369c195f1aa0bd3a40d40de3c48a
TE23B-Sandor-Lopez <sandor.lopez@elev.ga.ntig.se>
2024-11-20 14:44:50 +0100

    kugfhghj

---
+++ program.cs
@@ -0,0 +1,14 @@
+// string [] sak = ["du", "jag", "han", "hon", "dem"]
+
+new string [6];
+sak [1] = "du";
+sak [2] = "jag";
+sak [3] = "han";
+sak [4] = "hon";
+sak [5] = "dem";
+
+for (int i = 0; i < sak.Length; i++)
+{
+    Console.Writeline( sak[i] );
+}
+Console.Readline();
